04d4fa75a52f2ad893fed8ddafac2ceaee33df01
yibaben <yibaben@gmail.com>
2023-03-27 13:34:38 +0100

    student-controller

--- src/main/java/com/yibaben/SpringBootRestAPI/controller/StudentController.java
+++ src/main/java/com/yibaben/SpringBootRestAPI/controller/StudentController.java
@@ -1,2 +1,19 @@
-package com.yibaben.SpringBootRestAPI.controller;public class StudentController {
+package com.yibaben.SpringBootRestAPI.controller;
+
+import com.yibaben.SpringBootRestAPI.medel.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("student")
+    public Student getStudent() {
+        Student student = new Student(
+                1,
+                "Bennett",
+                "Wenifiri"
+        );
+        return student;
+    }
 }
--- src/main/java/com/yibaben/SpringBootRestAPI/medel/Student.java
+++ src/main/java/com/yibaben/SpringBootRestAPI/medel/Student.java
@@ -1,2 +1,17 @@
-package com.yibaben.SpringBootRestAPI.medel;public class Student {
+package com.yibaben.SpringBootRestAPI.medel;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Student {
+
+    private int id;
+    private String firstName;
+    private String lastName;
 }
