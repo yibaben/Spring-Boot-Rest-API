2dad87dcec27ac1f193160af46acf93fad4d1eee
yibaben <yibaben@gmail.com>
2023-03-27 16:49:13 +0100

    API with PostMapping and RequestBody

--- src/main/java/com/yibaben/SpringBootRestAPI/controller/StudentController.java
+++ src/main/java/com/yibaben/SpringBootRestAPI/controller/StudentController.java
@@ -1,10 +1,8 @@
 package com.yibaben.SpringBootRestAPI.controller;
 
 import com.yibaben.SpringBootRestAPI.medel.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,5 +51,17 @@ public class StudentController {
                                   @RequestParam String firstName,
                                   @RequestParam String lastName){
         return  new Student(id, firstName, lastName);
+    }
+
+    // Spring boot Rest API with HTTP Post Request
+    // @PostMapping and @RequestBody
+
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
     }
 }
